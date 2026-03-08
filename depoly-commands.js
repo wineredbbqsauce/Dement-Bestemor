a2c506201f4d986ac32f15e674f35d3ce3c22da2
Wineredbbqsauce <wineredbbqsauce@gmail.com>
2026-03-08 23:10:43 +0100

    Made it server wide usable

--- depoly-commands.js
+++ depoly-commands.js
@@ -13,13 +13,9 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 (async () => {
   try {
     console.log("Registering slash commands...");
-    await rest.put(
-      Routes.applicationCommands(
-        process.env.APPLICATION_ID,
-        process.env.GUILD_ID,
-      ),
-      { body: commands },
-    );
+    await rest.put(Routes.applicationCommands(process.env.APPLICATION_ID), {
+      body: commands,
+    });
     console.log("✅ Slash commands registered!");
   } catch (error) {
     console.error("Error registering slash commands:", error);
